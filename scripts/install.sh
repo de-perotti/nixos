c357f5e72b5388a3c2cf9774a25872b111b3f90d
Luigi Perotti <perottilds@gmail.com>
2025-05-05 19:17:32 -0300

    update

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 set -ex
 mkdir -p /mnt/etc/nixos
-ln -s configuration.nix /mnt/etc/nixos/configuration.nix
-ln -s hardware-configuration.nix /mnt/etc/nixos/hardware-configuration.nix
-ln -s nixos /mnt/etc/nixos/nixos
+cp -f configuration.nix /mnt/etc/nixos
+cp -f hardware-configuration.nix /mnt/etc/nixos/hardware-configuration.nix
+co -fr nixos /mnt/etc/nixos
 nixos-install
 nixos-enter --root /mnt -c 'passwd perotti'
