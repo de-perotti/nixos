2641105854537d5d5fdd3fd776c531467461af15
Luigi Perotti <perottilds@gmail.com>
2025-05-05 20:48:53 -0300

    update

--- modules/users/perotti.nix
+++ modules/users/perotti.nix
@@ -1,5 +1,17 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  environment.systemPackages = [
+    pkgs.zsh-powerlevel10k
+  ];
+  programs.zsh.enable = true;
+  programs.zsh.ohMyZsh.enable = true;
+  programs.zsh.ohMyZsh.plugins = [
+    "git"
+  ];
+  programs.zsh.promptInit = ''
+    source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme;
+  '';
   users.users.perotti.isNormalUser = true;
   users.users.perotti.extraGroups = [ "wheel" ];
+  users.users.perotti.shell = pkgs.zsh;
 }
