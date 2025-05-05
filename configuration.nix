7fb10a1ce897b38920193ae10c5360163386578c
perotti <perottilds@gmail.com>
2025-05-05 16:40:12 -0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,7 @@
 {
   imports = [./hardware-configuration.nix];
   boot.loader.systemd-boot.enable = true;
-  boot.loader.grub.device = nodev;
+  boot.loader.grub.device = "nodev";
   boot.loader.grub.efiSupport = true;
   system.stateVersion = "24.11";
 }
