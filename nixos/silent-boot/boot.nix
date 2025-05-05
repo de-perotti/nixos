0882988d4dd3e7fa3fb9346cd15e4ed8c51e785e
Luigi Perotti <perottilds@gmail.com>
2025-05-05 19:36:14 -0300

    update

--- nixos/silent-boot/boot.nix
+++ nixos/silent-boot/boot.nix
@@ -5,8 +5,4 @@
     boot.initrd.verbose = false;
     boot.loader.grub.device = "/dev/sda";
     boot.loader.grub.enable = true;
-    boot.loader.timeout = 0;
-    console.font = "ter-v32n";
-    console.keyMap = "us";
-    console.packages = [pkgs.terminus_font];
 }
