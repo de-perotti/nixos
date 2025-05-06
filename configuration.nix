0bfa42b32d73570f27ea9699fa0789c9bec2ff1d
Luigi Perotti <perottilds@gmail.com>
2025-05-06 00:38:28 -0300

    update

--- configuration.nix
+++ configuration.nix
@@ -13,58 +13,45 @@ in
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.enable = true;
 
+  #> Base
+  networking.hostName = "nixos-vm";
+  networking.networkmanager.enable = true;
+  time.timezone = "America/Sao_Paulo";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pt_BR.UTF-8";
+    LC_IDENTIFICATION = "pt_BR.UTF-8";
+    LC_MEASUREMENT = "pt_BR.UTF-8";
+    LC_MONETARY = "pt_BR.UTF-8";
+    LC_NAME = "pt_BR.UTF-8";
+    LC_NUMERIC = "pt_BR.UTF-8";
+    LC_PAPER = "pt_BR.UTF-8";
+    LC_TELEPHONE = "pt_BR.UTF-8";
+    LC_TIME = "pt_BR.UTF-8";
+  };
+
   #> Hyprland
-  environment.systemPackages = [
-    pkgs.hyprland
-    pkgs.xdg-utils
-    pkgs.xdg-desktop-portal
-    pkgs.xdg-desktop-portal-hyprland
-    pkgs.waybar
-    pkgs.dunst
-    pkgs.kitty
-    pkgs.wofi
-  ];
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-  programs.hyprland.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
-  services.displayManager.defaultSession = "hyprland";
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
-  hardware.graphics.enable = true;
-  fonts.packages = [
-    pkgs.nerdfonts
-    pkgs.noto-fonts
-    pkgs.noto-fonts-cjk-sans
-    pkgs.noto-fonts-emoji
-  ];
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.wayland.enable = true;
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+  };
+
+  #> Keyboard
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "intl";
+  };
+  console.keyMap = "us";
+
+  #> Printer
+  services.printing.enable = true;
 
   #> User: perotti
   users.users.perotti.isNormalUser = true;
-  users.users.perotti.extraGroups = [ "wheel" "networkmanager" "video" ];
-  home-manager.users.perotti = { pkgs, ... }: {
-    home.stateVersion = "24.11";
-    wayland.windowManager.hyprland.settings = {
-      "$mod" = "SUPER";
-      bind =
-        [
-          "$mod, F, exec, firefox"
-          ", Print, exec, grimblast copy area"
-        ]
-        ++ (
-          # workspaces
-          # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-          builtins.concatLists (builtins.genList (i:
-              let ws = i + 1;
-              in [
-                "$mod, code:1${toString i}, workspace, ${toString ws}"
-                "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-              ]
-            )
-            9)
-        );
-    };
-  };
+  users.users.perotti.extraGroups = [ "wheel" "networkmanager" ];
 
   #> Nix
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
