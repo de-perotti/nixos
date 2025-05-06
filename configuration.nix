f86e512789be5abae0448bb55e86e9cd43997ff6
Luigi Perotti <perottilds@gmail.com>
2025-05-05 21:12:27 -0300

    update

--- configuration.nix
+++ configuration.nix
@@ -1,10 +1,11 @@
-{ config, pkgs, ... }:
+{ ... }:
 {
   imports = [
     ./modules/boot/boot.nix
     ./hardware-configuration.nix
     ./modules/wayland/sway.nix
     ./modules/users/perotti.nix
-  ];
-  system.stateVersion = "24.11";
+    ./modules/nix/default.nix
+    ./modules/system/default.nix
+  ];  
 }
