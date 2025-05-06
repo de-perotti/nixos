8a40e76333bd0c9fbf65980ed5a1185b1d9d212d
Luigi Perotti <perottilds@gmail.com>
2025-05-05 23:22:51 -0300

    update

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -1,6 +1,12 @@
 { pkgs, ... }:
+let
+  home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz;
+in
 {
-  imports = [./hardware-configuration.nix];
+  imports = [
+    ./hardware-configuration.nix
+    (import "${home-manager}/nixos")
+  ];
 
   #> Boot
   boot.initrd.checkJournalingFS = true;
@@ -11,50 +17,42 @@
   time.timeZone = "America/Sao_Paulo";
 
   #> Programs
-  programs.hyprland.enable = true;
+  # programs.hyprland.enable = true;
 
   #> Packages
-  nixpkgs.config.allowUnfreePredicate = pkg:
-    builtins.elem (lib.getName pkg) [
-      "google-chome"
-      "jetbrains-toolbox"
-    ];
-  environment.systemPackages = [
-    pkgs.zsh-powerlevel10k
-    pkgs.waybar
-    pkgs.dunst
-    pkgs.libnotify
-    pkgs.swww
-    pkgs.terminator
-    pkgs.rofi-wayland
-    pkgs.jetbrains-toolbox
-    pkgs.google-chome
-    # (pkgs.waybar.overrideAttrs (oldAttrs: {
-    #     mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
-    #   })
-    # )
-  ];
+  # nixpkgs.config.allowUnfreePredicate = pkg:
+  #   builtins.elem (lib.getName pkg) [
+  #     "google-chome"
+  #     "jetbrains-toolbox"
+  #   ];
 
-  #> XDG
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [
-    pkgs.xdg-desktop-portal-gtk
-  ];
-
-  #> Zsh
-  # programs.zsh.enable = true;
-  # programs.zsh.ohMyZsh.enable = true;
-  # programs.zsh.ohMyZsh.plugins = [
-  #   "git"
-  # ];
-  # programs.zsh.promptInit = ''
-  #   source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme;
-  # '';
-
-  #> Users
+  #> User: perotti
   users.users.perotti.isNormalUser = true;
-  users.users.perotti.extraGroups = [ "wheel" ];
-  users.users.perotti.shell = pkgs.zsh;
+  # users.users.perotti.extraGroups = [ "wheel" ];
+  home-manager.users.perotti = { pkgs, ... }: {
+    home.stateVersion = "24.11";
+    home.packages = [
+      pkgs.zsh-powerlevel10k
+      # pkgs.terminator
+      # pkgs.jetbrains-toolbox
+      # pkgs.google-chome
+    ];
+    programs.git = {
+      enable = true;
+      userName  = "Luigi Perotti";
+      userEmail = "perottilds@gmail.com";
+    };
+    programs.zsh = {
+      enable = true;
+      ohMyZsh.enable = true;
+      ohMyZsh.plugins = [
+        "git"
+      ];
+      promptInit = ''
+        source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme;
+      '';
+    };
+  };
 
   #> Nix
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
