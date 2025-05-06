c8321abe21683b69419705a34bfb79469f7546c2
Luigi Perotti <perottilds@gmail.com>
2025-05-05 22:57:14 -0300

    update

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -1,22 +1,31 @@
 { pkgs, ... }:
 {
-  imports = [
-    # Boot
-    ./modules/boot/boot-virtualbox.nix
-
-    # Hardware
-    ./hardware-configuration.nix
-
-    # Programs
-    ./modules/programs/zsh.nix
-    ./modules/programs/hyprland.nix
-
-    # Users
-    ./modules/users/perotti.nix
-  ];
-
+  imports = [./hardware-configuration.nix];
+  #> Boot
+  boot.initrd.checkJournalingFS = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.enable = true;
+  #> Timezone
   time.timeZone = "America/Sao_Paulo";
-
+  #> Zsh
+  # environment.systemPackages = [
+  #   pkgs.zsh-powerlevel10k
+  # ];
+  # programs.zsh.enable = true;
+  # programs.zsh.ohMyZsh.enable = true;
+  # programs.zsh.ohMyZsh.plugins = [
+  #   "git"
+  # ];
+  # programs.zsh.promptInit = ''
+  #   source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme;
+  # '';
+  #> Users
+  users.users.perotti.isNormalUser = true;
+  users.users.perotti.extraGroups = [ "wheel" ];
+  # users.users.perotti.shell = pkgs.zsh;
+  #> Programs
+  programs.hyprland.enable = true;
+  #> Nix
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   system.stateVersion = "24.11";
 }
--- nix/modules/users/perotti.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs, ... }:
-{
-  users.users.perotti.isNormalUser = true;
-  users.users.perotti.extraGroups = [ "wheel" ];
-  users.users.perotti.shell = pkgs.zsh;
-}
