686afcb4546cc7a34cdc6568b6850ba5d050503f
Luigi Perotti <perottilds@gmail.com>
2025-05-08 10:39:33 -0300

    update

--- flake.nix
+++ flake.nix
@@ -10,23 +10,22 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
   outputs = inputs@{ nixpkgs, nixos-hardware, home-manager, ... }: {
-    nixosConfigurations = {
-      rice = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          nixos-hardware.nixosModules.common-pc-laptop-ssd
-          nixos-hardware.nixosModules.common-cpu-intel
-          nixos-hardware.nixosModules.common-gpu-intel
-          ./hardware-configuration.nix
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.perotti = import ./home.nix;
-          }
-        ];
+    nixosConfigurations.rice = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        nixos-hardware.nixosModules.common-pc-laptop-ssd
+        nixos-hardware.nixosModules.common-cpu-intel
+        nixos-hardware.nixosModules.common-gpu-intel
+        ./hardware-configuration.nix
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          extraSpecialArgs = { inherit inputs; }; 
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.perotti = import ./home.nix;
+        }
+      ];
       };
-    };
   };
 }
