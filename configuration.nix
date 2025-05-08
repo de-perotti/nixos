2ce2d8fb5200fba16c7c78a9fb3a7dae9498d8e1
Luigi Perotti <perottilds@gmail.com>
2025-05-08 09:18:22 -0300

    update

--- configuration.nix
+++ configuration.nix
@@ -4,8 +4,6 @@
 
 { config, pkgs, lib, inputs, ... }:
 {
-  imports = [ inputs.home-manager.nixosModules.default ];
-
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
@@ -76,97 +74,5 @@
     shell = pkgs.zsh;
   };
 
-  home-manager.useGlobalPkgs = true;
-  home-manager.useUserPackages = true;
-  home-manager.users.perotti = { pkgs, ... }: {
-    home.stateVersion = "24.11";
-    programs.home-manager.enable = true;
-  
-    home.username = "perotti";
-    home.homeDirectory = "/home/perotti";
-
-    fonts.fontconfig.enable = true;
-    fonts.fontconfig.defaultFonts = {
-      emoji = [ "Noto Color Emoji" ];
-      monospace = [ "JetBrains Mono" ];
-      sansSerif = [ "Noto Sans" ];
-      serif = [ "Noto Serif" ];
-    };
-  
-    home.packages = with pkgs; [
-      google-chrome
-      jetbrains-toolbox
-      neovim
-      docker
-      docker-compose
-      curl
-      wget
-      nerdfonts
-    ];
-
-    programs.htop.enable = true;
-    programs.fastfetch.enable = true;
-    
-
-    programs.foot.enable = true;
-    programs.foot.server.enable = true;
-    programs.foot.settings = {
-      main = {
-        font = "JetBrains Mono:size=13:weight=extrabold:line-height=1.2";
-        letter-spacing=0;
-        dpi-aware = false;
-      };
-      cursor = {
-        style = "block";
-        blink = true;
-      };
-      mouse = {
-        hide-when-typing = true;
-      };
-    };
-  
-    programs.zsh = { 
-      enable = true;
-      autosuggestion.enable = true;
-      syntaxHighlighting.enable = true;
-      dotDir = ".config/zsh";
-      history = {
-        path = "$HOME/.config/zsh/.zsh_history";
-        size = 10000;
-        save = 10000;
-      };
-      plugins = [
-        {
-          name = "powerlevel10k";
-          src = pkgs.zsh-powerlevel10k;
-          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-        }
-        {
-          name = "powerlevel10k-config";
-          src = ./p10k-config;
-          file = ".p10k.zsh";
-        }
-      ];
-      oh-my-zsh = {
-        enable = true;
-        plugins = [
-          "colorize"
-          "docker"
-          "docker-compose"
-          "git"
-          "emoji"
-          "history"
-          "node"
-          "npm"
-        ];
-      };
-    };
-  
-    programs.git.enable = true;
-    programs.git.userName = "de-perotti";
-    programs.git.userEmail = "perottilds@gmail.com";
-  };
-  
-  nix.settings.experimental-features = ["nix-command" "flakes"];
   system.stateVersion = "24.11";
 }
--- flake.nix
+++ flake.nix
@@ -9,13 +9,18 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
-  outputs = inputs @ { nixpkgs, ... }:  {
-    nixosConfigurations.rice = nixpkgs.lib.nixosSystem {
-      # system = "x86_64-linux";
-      specialArgs = inputs // {};
+  outputs = inputs@{ nixpkgs, ... }:  {
+    nixosConfigurations.rice = inputs.nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
       modules = [
-        ./hardware-configuration.nix
         ./configuration.nix
+        # inputs.home-manager.nixosModules.home-manager
+        # {
+        #   home-manager.useGlobalPkgs = true;
+        #   home-manager.useUserPackages = true;
+        #   home-manager.users.perotti = ./home.nix;
+        # }
+        ./hardware-configuration.nix
       ];
     };
   };
