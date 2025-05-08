4f4d14273ce7ce875a1b229d7cb69aedf82d4fb5
Luigi Perotti <perottilds@gmail.com>
2025-05-08 11:08:12 -0300

    update

--- flake.nix
+++ flake.nix
@@ -5,26 +5,26 @@
   };
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
-  outputs = { nixpkgs, nixos-hardware, home-manager, ... }: {
+  outputs =  { nixpkgs, nixos-hardware, home-manager, ... }:  {
     nixosConfigurations.rice = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        # nixos-hardware.nixosModules.common-pc-laptop-ssd
-        # nixos-hardware.nixosModules.common-cpu-intel
-        # nixos-hardware.nixosModules.common-gpu-intel
+        nixos-hardware.nixosModules.common-pc-laptop-ssd
+        nixos-hardware.nixosModules.common-cpu-intel
+        nixos-hardware.nixosModules.common-gpu-intel
         ./hardware-configuration.nix
         ./configuration.nix
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.users.perotti = import ./home.nix;
+          home-manager.users.perotti = ./home.nix;
         }
       ];
-      };
+    };
   };
 }
--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }: {
+{ pkgs, ... }: {
   home.stateVersion = "24.11";
   programs.home-manager.enable = true;
 
