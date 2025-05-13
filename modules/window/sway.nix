103e8b400efcc2d3e380b340c904d6ab222c7261
de-perotti <perottilds@gmail.com>
2025-05-13 13:28:52 -0300

    update

--- modules/window/sway.nix
+++ modules/window/sway.nix
@@ -1,8 +1,6 @@
 { ... }: {
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
-  # services.xserver.displayManager.gdm.enable = false;
-  # services.xserver.displayManager.gdm.wayland = true;
   programs.sway.enable = true;
   programs.xwayland.enable = true;
   security.polkit.enable = true;
