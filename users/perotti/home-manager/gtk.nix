59c7f4dbbf67771865e57a0ee1a849753b42ed2e
Luigi Perotti <perottilds@gmail.com>
2025-05-15 10:26:53 -0300

    update

--- users/perotti/home-manager/gtk.nix
+++ users/perotti/home-manager/gtk.nix
@@ -1,11 +1,5 @@
 { pkgs, ... }: {
-  # home.pointerCursor.enable = true;
-  # home.pointerCursor.gtk.enable = true;
   home.pointerCursor.name = "Vanilla-DMZ";
   home.pointerCursor.package = pkgs.vanilla-dmz;
-  # gtk.enable = true;
-  # gtk.cursorTheme = {
-  #   package = pkgs.vanilla-dmz;
-  #   name = "Vanilla-DMZ";
-  # };
+  home.pointerCursor.size = 32;
 }
