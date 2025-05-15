72dd384e04bf1f054246ade5b5b5f7674d416b87
Luigi Perotti <perottilds@gmail.com>
2025-05-15 07:16:24 -0300

    update

--- users/perotti/sway.nix
+++ users/perotti/sway.nix
@@ -3,6 +3,13 @@
   programs.xwayland.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
   programs.regreet.enable = true;
+  programs.regreet.font.package = pkgs.jetbrains-mono;
+  programs.regreet.settings = {
+    background.path = ./home-manager/config/wallpaper.jpg;
+    GTK.font_name = "JetBrainsMono Nerd Font Mono 16";
+    appearance.greeting_msg = "FYMUG?";
+    widget.clock.timezone = "America/Sao_Paulo";
+  };
   services.greetd.enable = true;
   security.polkit.enable = true;
 }
