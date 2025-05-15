6279ec7b8dd37a9e0c747d752cf3b01972be4084
Luigi Perotti <perottilds@gmail.com>
2025-05-15 08:39:36 -0300

    update

--- users/perotti/sway.nix
+++ users/perotti/sway.nix
@@ -4,7 +4,7 @@
   services.xserver.displayManager.gdm.wayland = true;
   services.greetd.enable = true;
   services.greetd.settings = {
-    default_session.command = "${pkgs.cage}/bin/cage -s -- wlgreet";
+    default_session.command = "${pkgs.cage}/bin/cage -s -- ${pkgs.greetd.wlgreet}/bin/wlgreet";
   };
   security.polkit.enable = true;
 }
