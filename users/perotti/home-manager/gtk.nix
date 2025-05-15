67fe3e43559de35487e6221bf824577b6e8e8808
Luigi Perotti <perottilds@gmail.com>
2025-05-15 11:08:19 -0300

    update

--- users/perotti/home-manager/gtk.nix
+++ users/perotti/home-manager/gtk.nix
@@ -1,8 +1,11 @@
 { pkgs, ... }: {
+  gtk.enable = true;
   home.pointerCursor = {
     name = "phinger-cursors-dark";
     package = pkgs.phinger-cursors;
     size = 64;
     gtk.enable = true;
+    x11.enable = true;
+    # sway.enable = true;
   };
 }
