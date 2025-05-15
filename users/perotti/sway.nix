5f7b9ddf1b593570c955af635b131e269f288af5
Luigi Perotti <perottilds@gmail.com>
2025-05-15 09:36:37 -0300

    update

--- users/perotti/sway.nix
+++ users/perotti/sway.nix
@@ -2,25 +2,11 @@
   programs.sway.enable = true;
   programs.sway.wrapperFeatures.gtk = true;
   programs.xwayland.enable = true;
-  # services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
   services.xserver.displayManager.gdm.autoSuspend = false;
   services.greetd.enable = true;
   services.greetd.settings = {
-    default_session = {
-      command = ''
-        ${pkgs.greetd.tuigreet}/bin/tuigreet \
-          --time \
-          --greet-align left \
-          --power-shutdown 'systemctl poweroff' \
-          --power-reboot 'systemctl reboot' \
-          --window-padding 0 \
-          --prompt-padding 1 \
-          --container-padding 1 \
-          --theme container=black;input=red \
-          --cmd sway
-      '';
-    };
+    default_session.command = "${pkgs.greetd.greetd}/bin/agreety --cmd sway";
   };
   security.polkit.enable = true;
 }
