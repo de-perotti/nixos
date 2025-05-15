dc7932444d1beac3a0e6be2ef6bc0dc07005a224
Luigi Perotti <perottilds@gmail.com>
2025-05-15 11:18:47 -0300

    update

--- users/perotti/home-manager/sway.nix
+++ users/perotti/home-manager/sway.nix
@@ -4,7 +4,7 @@
     "default" = {
       theme = "native";
       blocks = [
-        { block = "sound"; driver = "pulseaudio"; format = " \${volume|🔇} "; max_vol = "100"; }
+        { block = "sound"; driver = "pulseaudio"; format = " {$volume|🔇} "; max_vol = "100"; }
         { block = "backlight"; }
         { block = "net"; }
         { block = "battery"; }
