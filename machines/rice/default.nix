f6b9baeee818c9c3f853812b61ec97277b969bf1
Luigi Perotti <perottilds@gmail.com>
2025-05-15 13:16:49 -0300

    hardware tweak

--- machines/rice/default.nix
+++ machines/rice/default.nix
@@ -6,8 +6,9 @@
       ../../hardware-configuration.nix
       nixos-hardware.nixosModules.common-cpu-intel
       nixos-hardware.nixosModules.common-gpu-intel
+      nixos-hardware.nixosModules.common-pc
       nixos-hardware.nixosModules.common-pc-laptop
-      nixos-hardware.nixosModules.lenovo-ideapad-s145-15api
+      nixos-hardware.nixosModules.common-pc-laptop-ssd
       {
         boot.loader.systemd-boot.enable = true;
         boot.loader.efi.canTouchEfiVariables = true;
