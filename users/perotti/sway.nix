1c1006790007b02d962d4920c92460b6cb15f9ca
Luigi Perotti <perottilds@gmail.com>
2025-05-15 07:17:22 -0300

    update

--- users/perotti/sway.nix
+++ users/perotti/sway.nix
@@ -1,10 +1,10 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   programs.sway.enable = true;
   programs.xwayland.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
   programs.regreet.enable = true;
   programs.regreet.font.package = pkgs.jetbrains-mono;
-  programs.regreet.settings = {
+  programs.regreet.settings = lib.mkDefault {
     background.path = ./home-manager/config/wallpaper.jpg;
     GTK.font_name = "JetBrainsMono Nerd Font Mono 16";
     appearance.greeting_msg = "FYMUG?";
