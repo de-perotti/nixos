70e8438021d5f19966c774399bd8770a3f71281b
Luigi Perotti <perottilds@gmail.com>
2025-05-15 09:05:05 -0300

    update

--- users/perotti/sway.nix
+++ users/perotti/sway.nix
@@ -2,8 +2,10 @@
   programs.sway.enable = true;
   programs.sway.wrapperFeatures.gtk = true;
   programs.xwayland.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
   services.xserver.displayManager.gdm.autoSuspend = false;
+  programs.regreet.enable = true;
+  programs.regreet.package = pkgs.greetd.wlgreet;
   security.polkit.enable = true;
 }
