a44fe2b4999261bed22bad5ced606f3aae020229
Luigi Perotti <perottilds@gmail.com>
2025-05-15 07:04:38 -0300

    update

--- users/perotti/sway.nix
+++ users/perotti/sway.nix
@@ -2,10 +2,10 @@
   programs.sway.enable = true;
   programs.xwayland.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
-  # programs.regreet.enable = true;
+  programs.regreet.enable = true;
   services.greetd.enable = true;
   services.greetd.settings = {
-    default_session.command = "${pkgs.greetd.greetd}/bin/agreety --cmd sway";
+    default_session.command = "${pkgs.greetd.regreet}/bin/regreet --cmd sway";
   };
   security.polkit.enable = true;
 }
