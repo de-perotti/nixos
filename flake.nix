acd65ae94fa9ef96f44ccbed8a9b58bf172d4245
Luigi Perotti <perottilds@gmail.com>
2025-05-16 08:41:55 -0300

    update

--- flake.nix
+++ flake.nix
@@ -8,9 +8,32 @@
     nixpkgs.url = "github:NixOS/nixpkgs/master";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    systems.url = "github:nix-systems/x86_64-linux";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.systems.follows = "systems";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
-  outputs = inputs: {
-    nixosConfigurations.rice = import ./machines/rice inputs;
-  };
+  outputs = inputs@{ flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      nixosConfigurations.rice = import ./nixos/rice inputs;
+      devShells.default = let
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+        with input.pkgs; mkShell {
+          buildInputs = [
+                openssl
+                pkg-config
+                eza
+                fd
+                rust-bin.beta.latest.default
+              ];
+
+              shellHook = ''
+                alias ls=eza
+                alias find=fd
+              '';
+        };
+    });
 }
