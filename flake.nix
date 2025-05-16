99fd03ad764ccfcf062e058d03bb96368ebac8a6
de-perotti <perottilds@gmail.com>
2025-05-16 05:02:46 -0300

    update

--- flake.nix
+++ flake.nix
@@ -5,8 +5,8 @@
     warn-dirty = false;
   };
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
