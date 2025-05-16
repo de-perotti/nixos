c3e70c11add6ec019625298315b543b5a906d70b
perotti <perottilds@gmail.com>
2025-05-16 09:46:53 -0300

    feat: flakes flakes (#2)

--- flake.nix
+++ flake.nix
@@ -5,12 +5,22 @@
     warn-dirty = false;
   };
   inputs = {
+    systems.url = "github:nix-systems/default";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.systems.follows = "systems";
+
     nixpkgs.url = "github:NixOS/nixpkgs/master";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
-  outputs = inputs: {
-    nixosConfigurations.rice = import ./machines/rice inputs;
-  };
+  outputs = inputs @ { flake-utils, nixpkgs, ... }:
+    with flake-utils; lib.eachSystem [ lib.system.x86_64-linux ] (system: let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        nixosConfigurations = import ./nixos/rice inputs;
+        devShells.default = import ./shells/nodejs_22.nix { inherit pkgs; };
+      }
+    );
 }
