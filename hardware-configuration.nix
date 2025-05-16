95678eee0d7926d23eaaad8e09f507eee6d18060
Luigi Perotti <perottilds@gmail.com>
2025-05-16 09:48:38 -0300

    update

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -1,5 +1,7 @@
 {...}: {
-  
+  # >! This file is a placeholder !<
+  # Hardware Configuration is obtained at runtime
+  # on the target host
   fileSystems."/" = {
     device = "placeholder";
     fsType = "ext4";
