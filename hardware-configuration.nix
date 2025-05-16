6b4f39bb9ffe71e1f5433cd14c2d277d1562199a
Luigi Perotti <perottilds@gmail.com>
2025-05-16 09:49:03 -0300

    update

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -2,6 +2,7 @@
   # >! This file is a placeholder !<
   # Hardware Configuration is obtained at runtime
   # on the target host
+  # eg: sudo nixos-generate-config --show-hardware-config > hardware-configuration.nix
   fileSystems."/" = {
     device = "placeholder";
     fsType = "ext4";
