70bdb20cfbff03f53b1d1d51607478ed90fd3ced
Luigi Perotti <perottilds@gmail.com>
2025-05-16 09:46:38 -0300

    update

--- switch-rice.sh
+++ switch-rice.sh
@@ -1,4 +1,4 @@
 set -ex
 sudo nixos-generate-config --show-hardware-config > hardware-configuration.nix
-sudo nixos-rebuild switch --flake .#x86_64-linux
+sudo nixos-rebuild switch --flake .#rice
 git checkout -- hardware-configuration.nix
